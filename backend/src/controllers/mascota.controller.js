8b6fc80b674de800dbd10fd7542291fde585ee70
Wolf <gabriel.guzman2201@alumnos.ubiobio.cl>
2024-07-05 19:32:21 -0400

    Actualizacion de Mascota controler(Posiblemente final)

--- backend/src/controllers/mascota.controller.js
+++ backend/src/controllers/mascota.controller.js
@@ -36,3 +36,105 @@ export async function createMascota(req, res) {
 }
 
 };
+
+export async function DeleteMascota(req, res){
+    try{
+        const idMascota = req.query.rut;
+        if (!idMascota) {
+            res.status(400).json({
+                message: "El parámetro 'id' es requerido.",
+                data: null
+            });
+            return;
+        }
+
+        const mascota = await Mascota.findOneAndDelete({ id: idMascota });
+
+        if (!mascota) {
+            return res.status(404).json({
+                message: "Mascota no encontrada",
+                data: null
+            });
+        }
+
+        res.status(200).json({
+            message: "Mascota eliminada exitosamente!",
+            data: mascota
+        });
+    }catch(error){
+        console.log("Error en mascota.controller.js -> DeleteMascota():", error);
+        res.status(500).json({ message: "Error interno del servidor." });
+    }
+}
+
+export async function GetMascota(req, res){
+    try{
+        const idMascotas = req.query.id;
+
+        if (!idMascotas) {
+            res.status(400).json({
+                message: "El parámetro 'id' es requerido.",
+                data: null
+            });
+            return;
+        }
+
+        const mascota = await Mascota.findOne({id: idMascotas});
+
+        if(!mascota){
+            res.status(404).json({
+                message: "Mascota no encontrada",
+                data: null
+            })
+            return;
+        }
+    }catch(error){
+        console.log("Error en mascota.controller.js -> GetMascota():", error);
+        res.status(500).json({ message: "Error interno del servidor." });
+    }
+}
+
+export async function GetMascotas(req, res){
+    try{
+        const mascotas = await Mascotas.find().populate('id', 'nombre');
+        res.status(200).json({
+            message: "Lista de Mascotas",
+            data: mascotas
+        });
+    }catch(error){
+        console.log("Error en mascota.controller.js -> GetMascotas():", error);
+        res.status(500).json({ message: "Error interno del servidor." });
+    }
+}
+
+export async function UpdateMascota(req, res){
+    try{
+        const idMascota = req.query.id;
+        const updatedData = req.body;
+
+        if (!idMascota) {
+            res.status(400).json({
+                message: "El parámetro 'id' es requerido.",
+                data: null
+            });
+            return;
+        }
+        const MascotaMod = await Mascota.findOneAndUpdate({ id: idMascota }, updatedData, { new: true });
+
+        if (!MascotaMod) {
+            res.status(404).json({
+                message: "Mascota no encontrado",
+                data: null
+            });
+            return;
+        }
+
+        res.status(200).json({
+            message: "Mascota actualizado correctamente!",
+            data: MascotaMod
+        });
+    }catch(error){
+        console.log("Error en mascota.controller.js -> UpdateMascota():", error);
+        res.status(500).json({ message: "Error interno del servidor." });
+    }
+}
