17ad019230300bf74a583608f3e6dc1ec3d76c35
Gustavo <gustavo.machuca2201@alumnos.ubiobio.cl>
2024-07-05 17:09:53 -0400

    v2 model, v1 controller

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -8,5 +8,15 @@ import Comment from "../models/comment.model.js";
 //  Funciones
 
 export async function create(req,res){
-    
+    try {
+        const comment = await comment.create(req.body);
+        res.status(201).json({
+            message: "Comentario creado",
+            data: comment
+        });
+    }catch(error){
+        console.log("Error en comment.controller.js -> create(): ", error);
+        res.status(500).json({ message: error.message });
+    }
 }
+
