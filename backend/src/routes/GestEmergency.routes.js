e1450743f752f3dff9409dfad8893d59392711bb
jeremi <jeremi.munoz2201@alumnos.ubiobio.cl>
2024-07-07 14:21:52 -0400

    MOD GestEmergency.routes.js inserción de algunos comentarios

--- backend/src/routes/GestEmergency.routes.js
+++ backend/src/routes/GestEmergency.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { getEncargados, createEncargado, updateEncargado, deleteEncargado,
-    getVeterinarias, createVeterinaria, updateVeterinaria, deleteVeterinaria } from "../controllers/info.controller";
+    getVeterinarias, createVeterinaria, updateVeterinaria, deleteVeterinaria } from "../controllers/info.controller.js";
 import { isAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -8,13 +8,13 @@ const router = Router();
 // Rutas para encargados
 router.get("/encargados", isAdmin, getEncargados);
 router.post("/encargados", isAdmin, createEncargado);
-router.put("/encargados/:id", isAdmin, updateEncargado);
+router.put("/encargados/:id", isAdmin, updateEncargado); // Protegida con isAdmin
 router.delete("/encargados/:id", isAdmin, deleteEncargado);
 
 // Rutas para veterinarias
 router.get("/veterinarias", isAdmin, getVeterinarias);
 router.post("/veterinarias", isAdmin, createVeterinaria);
-router.put("/veterinarias/:id", isAdmin, updateVeterinaria);
+router.put("/veterinarias/:id", isAdmin, updateVeterinaria); // Protegida con isAdmin
 router.delete("/veterinarias/:id", isAdmin, deleteVeterinaria);
 
 export default router;
