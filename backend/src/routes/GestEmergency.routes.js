3f750d754426db2d5598b6480a0032f988ae107a
jeremi <jeremi.munoz2201@alumnos.ubiobio.cl>
2024-07-07 20:32:04 -0400

    MOD la estructura de el archivo: GestEmergency.routes.js

--- backend/src/routes/GestEmergency.routes.js
+++ backend/src/routes/GestEmergency.routes.js
@@ -1,20 +1,29 @@
+//importar el modulo express
 import { Router } from "express";
+
 import { getEncargados, createEncargado, updateEncargado, deleteEncargado,
     getVeterinarias, createVeterinaria, updateVeterinaria, deleteVeterinaria } from "../controllers/info.controller.js";
-import { isAdmin } from "../middlewares/auth.middleware.js";
+
+// Middlewares de autorizacion
+import { isAdmin, isUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 // Rutas para encargados
-router.get("/encargados", isAdmin, getEncargados);
-router.post("/encargados", isAdmin, createEncargado);
-router.put("/encargados/:id", isAdmin, updateEncargado); // Protegida con isAdmin
+router.get("/encargados", isUser, getEncargados); // Visualización de los datos (usuarios)
+
+// Protegida con isAdmin
+router.post("/encargados", isAdmin, createEncargado); 
+router.put("/encargados/:id", isAdmin, updateEncargado); 
 router.delete("/encargados/:id", isAdmin, deleteEncargado);
 
+
 // Rutas para veterinarias
-router.get("/veterinarias", isAdmin, getVeterinarias);
-router.post("/veterinarias", isAdmin, createVeterinaria);
-router.put("/veterinarias/:id", isAdmin, updateVeterinaria); // Protegida con isAdmin
+router.get("/veterinarias", isUser, getVeterinarias); // Visualización de los datos (usuarios)
+
+// Protegida con isAdmin
+router.post("/veterinarias", isAdmin, createVeterinaria); 
+router.put("/veterinarias/:id", isAdmin, updateVeterinaria); 
 router.delete("/veterinarias/:id", isAdmin, deleteVeterinaria);
 
 export default router;
